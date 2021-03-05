bf08ae66f1bd0938fc57358391dd66ae6bfedc20
jbeedle19 <josh.beedle@gmail.com>
2021-03-05 13:49:32 -0500

    created User schema

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+// Schema for the User model
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: 'Please include a username!',
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: 'Please include an email address!',
+            match: [/.+\@.+\..+/, 'Please enter a valid email address!'],
+            unique: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
