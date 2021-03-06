826b7703d1329b32b883ce03463802d202df152e
jbeedle19 <josh.beedle@gmail.com>
2021-03-05 21:18:27 -0500

    outlined userController

---
+++ controllers/user-controller.js
@@ -0,0 +1,14 @@
+const { User } = require('../models');
+
+const userController = {
+    // Get all Users
+    // Get one User by ID
+    // Create a new User
+    // Update a User by ID
+    // Delete a User by ID
+        //Bonus - remove associated thoughts when deleted
+    // Add a new friend to User's friend list with POST
+    // Delete a friend from User's friend list
+};
+
+module.exports = userController;
