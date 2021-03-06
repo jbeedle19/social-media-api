befb9a0162ac4a77f5dbb2e2659d15b679a162de
jbeedle19 <josh.beedle@gmail.com>
2021-03-06 09:52:41 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencode({ extended: true }));
+// Use the follwoing when front end is connected
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
